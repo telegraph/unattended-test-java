d1738fb09c08018bbe6950fcd0c7255d8b30aa71
Ather Mughal <ather.mughal@telegraph.co.uk>
2018-09-17 09:41:38 +0100

    added sample test

--- src/test/java/telegraph/articles/models/ArticleTest.java
+++ src/test/java/telegraph/articles/models/ArticleTest.java
@@ -2,6 +2,12 @@ package telegraph.articles.models;
 
 import static org.junit.Assert.*;
 
+import org.junit.Test;
+
 public class ArticleTest {
 
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
 }
